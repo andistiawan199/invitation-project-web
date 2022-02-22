136f42e2ed5cb547294fae893f567c474e64f77a
Andi Stiawan <andistiawan199@gmail.com>
2022-02-22 16:17:21 +0700

    test groom and bride name

--- src/Main.js
+++ src/Main.js
@@ -51,10 +51,10 @@ export default class Main extends Component {
                     <div class="col-md-8 col-md-offset-2 text-center">
                       <div class="display-t">
                         <div class="display-tc animate-box" data-animate-effect="fadeIn">
-                          <h1>Joefrey &amp; Sheila</h1>
-                          <h2>We Are Getting Married</h2>
+                          <h1>Hendriansyah &amp; Sri</h1>
+                          <h2>Undangan Pernikahan</h2>
                           <div class="simply-countdown simply-countdown-one"></div>
-                          <p><a href="/#" class="btn btn-default btn-sm">Save the date</a></p>
+                          <p><a href="#fh5co-couple" class="btn btn-default btn-sm">Buka Undangan</a></p>
                         </div>
                       </div>
                     </div>
@@ -66,9 +66,9 @@ export default class Main extends Component {
                 <div class="container">
                   <div class="row">
                     <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-                      <h2>Hello!</h2>
-                      <h3>November 28th, 2016 New York, USA</h3>
-                      <p>We invited you to celebrate our wedding</p>
+                      <h2>Kepada Yth. Andi Stiawan</h2>
+                      <h3>Bandung, 22 Februari 2022</h3>
+                      <p>Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud untuk mengundang saudara untuk dapat menghadiri Akad Nikah kami</p>
                     </div>
                   </div>
                   <div class="couple-wrap animate-box">
@@ -77,8 +77,8 @@ export default class Main extends Component {
                         <img src="images/groom.jpg" alt="groom" class="img-responsive"/>
                       </div>
                       <div class="desc-groom">
-                        <h3>Joefrey Mahusay</h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+                        <h3>Hendriansyah</h3>
+                        <p>Putra ke-2 dari Bpk. Nama Bapaknya Cume dan Ibu. Nama Ibunya Cume</p>
                       </div>
                     </div>
                     <p class="heart text-center"><i class="icon-heart2"></i></p>
@@ -87,8 +87,8 @@ export default class Main extends Component {
                         <img src="images/bride.jpg" alt="groom" class="img-responsive"/>
                       </div>
                       <div class="desc-bride">
-                        <h3>Sheila Mahusay</h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+                        <h3>Sri</h3>
+                        <p>Putr1 ke-2 dari Bpk. Nama Bapaknya Sri dan Ibu. Nama Ibunya Sri</p>
                       </div>
                     </div>
                   </div>
